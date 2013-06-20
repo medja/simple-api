3bce204f7076517cc615dbce00bebad7295466e0
Metod Medja <metod@medja.net>
2013-06-20 09:54:30 +0000

    Replace single quotes with double quotes

--- api.php
+++ api.php
@@ -7,9 +7,9 @@ class api
 	public function __construct($regex, $callback, $middleware = array())
 	{
 		$this->regex = $regex;
-		if (preg_match_all('/\{([\w\_]+)[\?]?\}/', $regex, $matches))
+		if (preg_match_all("/\{([\w\_]+)[\?]?\}/", $regex, $matches))
 			foreach ($matches[1] as $match)
-				$this->where[$match] = '.+';
+				$this->where[$match] = ".+";
 		$this->callback = $callback;
 		$this->middleware = $middleware;
 	}
@@ -17,17 +17,17 @@ class api
 	public function where($name, $regex)
 	{
 		if (!isset($this->where[$name]))
-			throw new Exception('Parameter not defined!');
+			throw new Exception("Parameter not defined!");
 		$this->where[$name] = $regex;
 		return $this;
 	}
 	
 	public function match($url)
 	{
-		if (!preg_match_all('/^' .  preg_replace_callback('/(\\\?.)\\\{([^\}]+)\\\}/i', function($matches) {
-			return ($matches[1] != '?' ? ('(' . $matches[1] . '|' . $matches[1]) : ($matches[1] . '('))
-				. '(' . $this->where[$matches[2]]. '))?';
-		}, '\/?' . preg_quote($this->regex, '/')) . '\/?$/i', $url, $matches, PREG_SET_ORDER)) return false;
+		if (!preg_match_all("/^" .  preg_replace_callback("/(\\\?.)\\\{([^\}]+)\\\}/i", function($matches) {
+			return ($matches[1] != "?" ? ("(" . $matches[1] . "|" . $matches[1]) : ($matches[1] . "("))
+				. "(" . $this->where[$matches[2]]. "))?";
+		}, "\/?" . preg_quote($this->regex, "/")) . "\/?$/i", $url, $matches, PREG_SET_ORDER)) return false;
 		$this->data = array();
 		$this->defaults = array();
 		$this->info = new ReflectionFunction($this->callback);
@@ -53,7 +53,7 @@ class api
 		foreach ($info->getParameters() as $parameter)
 		{
 			$i = array_search($parameter->name, $this->keys);
-			if ($i === false) throw new Exception('Parameter not defined!');
+			if ($i === false) throw new Exception("Parameter not defined!");
 			if (in_array($parameters->name, $this->defaults) && $parameter->isDefaultValueAvailable())
 				$args[] = $parameter->getDefaultValue();
 			else $args[] = $this->data[$i];
@@ -66,9 +66,9 @@ class api
 		foreach ($this->middleware as $middleware)
 		{
 			if (is_bool($middleware)) {
-				if (!$middleware) return array('status' => 401);
+				if (!$middleware) return array("status" => 401);
 			} else if (!$this->invoke($middleware))
-				return array('status' => 401);
+				return array("status" => 401);
 		}
 		return self::make($this->info->invokeArgs($this->data));
 	}
@@ -79,10 +79,10 @@ class api
 	{
 		$cargs = count($args);
 		if ($cargs < 2 || !is_string($args[0]) || !is_callable($args[$cargs - 1]))
-			throw new Exception('Invlaid argument format!');
+			throw new Exception("Invlaid argument format!");
 		for ($i = 0; $i < $cargs - 2; $i++)
 			if (!(is_bool($args[$i] || is_callable($args[$i]))))
-				throw new Exception('Invlaid argument format!');
+				throw new Exception("Invlaid argument format!");
 		$listener = new self($args[0], $args[$cargs - 1], array_slice($args, 1, -1));
 		self::$listeners[strtolower($name)][] = $listener;
 		return $listener;
@@ -90,25 +90,25 @@ class api
 	
 	private static function make($data)
 	{
-		if (is_int($data) && $data > 399 && $data < 416) return array('status' => $data);
+		if (is_int($data) && $data > 399 && $data < 416) return array("status" => $data);
 		if (is_object($data)) $data = (array)$data;
 		if (!is_array($data)) $data = array($data);
-		else if (isset($data['status']) && isset($data['response'])) return $data;
-		return array('status' => 200, 'response' => $data);
+		else if (isset($data["status"]) && isset($data["response"])) return $data;
+		return array("status" => 200, "response" => $data);
 	}
 	
 	private static function emit($url, $method)
 	{
 		if (!isset(self::$listeners[$method]) || !is_array(self::$listeners[$method]))
-			return array('status' => 404);
+			return array("status" => 404);
 		foreach(self::$listeners[$method] as $listener)
 			if ($listener->match($url))
 				return $listener->execute();
-		return array('status' => 404);
+		return array("status" => 404);
 	}
 	
-	private static $paths = array('PATH_INFO', 'ORIG_PATH_INFO', 'REQUEST_URI', 'SCRIPT_NAME'
-		, 'SCRIPT_NAME', 'PATH_TRANSLATED', 'PHP_SELF', 'QUERY_STRING');
+	private static $paths = array("PATH_INFO", "ORIG_PATH_INFO", "REQUEST_URI", "SCRIPT_NAME"
+		, "SCRIPT_NAME", "PATH_TRANSLATED", "PHP_SELF", "QUERY_STRING");
 	
 	private static function url()
 	{
@@ -120,16 +120,16 @@ class api
 		if (self::$called) return;
 		self::$called = true;
 		if ($url == null) $url = self::url();
-		if ($method == null) $method = $_SERVER['REQUEST_METHOD'];
+		if ($method == null) $method = $_SERVER["REQUEST_METHOD"];
 		$method = strtolower($method);
 		$output = self::emit($url, $method);
-		http_response_code($output['status']);
-		header('Content-Type: application/json; charset=utf-8');
-		if (isset($output['response']))
-			echo json_encode($output['response']);
+		http_response_code($output["status"]);
+		header("Content-Type: application/json; charset=utf-8");
+		if (isset($output["response"]))
+			echo json_encode($output["response"]);
 	}
 }
 
-register_shutdown_function(array('api', 'process'));
+register_shutdown_function(array("api", "process"));
 
 ?>
