b07955fa887a88b286366868d6f54658959fe3d7
Metod Medja <metod@medja.net>
2013-06-22 23:00:06 +0200

    Revert to single quotes

--- api.php
+++ api.php
@@ -7,9 +7,9 @@ class api
 	public function __construct($regex, $callback, $middleware = array())
 	{
 		$this->regex = $regex;
-		if (preg_match_all("/\{([\w\_]+)[\?]?\}/", $regex, $matches))
+		if (preg_match_all('/\{([\w\_]+)[\?]?\}/', $regex, $matches))
 			foreach ($matches[1] as $match)
-				$this->where[$match] = ".+";
+				$this->where[$match] = '.+';
 		$this->callback = $callback;
 		$this->middleware = $middleware;
 	}
@@ -17,17 +17,17 @@ class api
 	public function where($name, $regex)
 	{
 		if (!isset($this->where[$name]))
-			throw new Exception("Parameter not defined!");
+			throw new Exception('Parameter not defined!');
 		$this->where[$name] = $regex;
 		return $this;
 	}
 	
 	public function match($url)
 	{
-		if (!preg_match_all("/^" .  preg_replace_callback("/(\\\?.)\\\{([^\}]+)\\\}/i", function($matches) {
-			return ($matches[1] != "?" ? ("(" . $matches[1] . "|" . $matches[1]) : ($matches[1] . "("))
-				. "(" . $this->where[$matches[2]]. "))?";
-		}, "\/?" . preg_quote($this->regex, "/")) . "\/?$/i", $url, $matches, PREG_SET_ORDER)) return false;
+		if (!preg_match_all('/^' .  preg_replace_callback('/(\\\?.)\\\{([^\}]+)\\\}/i', function($matches) {
+			return ($matches[1] != '?' ? ('(' . $matches[1] . '|' . $matches[1]) : ($matches[1] . '('))
+				. '(' . $this->where[$matches[2]]. '))?';
+		}, '\/?' . preg_quote($this->regex, '/')) . '\/?$/i', $url, $matches, PREG_SET_ORDER)) return false;
 		$this->data = array();
 		$this->defaults = array();
 		$this->info = new ReflectionFunction($this->callback);
@@ -53,7 +53,7 @@ class api
 		foreach ($info->getParameters() as $parameter)
 		{
 			$i = array_search($parameter->name, $this->keys);
-			if ($i === false) throw new Exception("Parameter not defined!");
+			if ($i === false) throw new Exception('Parameter not defined!');
 			if (in_array($parameters->name, $this->defaults) && $parameter->isDefaultValueAvailable())
 				$args[] = $parameter->getDefaultValue();
 			else $args[] = $this->data[$i];
@@ -77,7 +77,7 @@ class api
 	{
 		$cargs = count($args);
 		if ($cargs < 2)
-			throw new Exception("Invlaid argument format!");
+			throw new Exception('Invlaid argument format!');
 		$listener = new self($args[0], $args[$cargs - 1], array_slice($args, 1, -1));
 		self::$listeners[strtolower($name)][] = $listener;
 		return $listener;
@@ -96,11 +96,11 @@ class api
 			foreach(self::$listeners[$method] as $listener)
 				if ($listener->match($url))
 					return self::make($listener->execute());
-		self::$response = "404";
+		self::$response = '404';
 	}
 	
-	private static $paths = array("PATH_INFO", "ORIG_PATH_INFO", "REQUEST_URI", "SCRIPT_NAME"
-		, "SCRIPT_NAME", "PATH_TRANSLATED", "PHP_SELF", "QUERY_STRING");
+	private static $paths = array('PATH_INFO', 'ORIG_PATH_INFO', 'REQUEST_URI', 'SCRIPT_NAME'
+		, 'SCRIPT_NAME', 'PATH_TRANSLATED', 'PHP_SELF', 'QUERY_STRING');
 	
 	private static function url()
 	{
@@ -112,13 +112,13 @@ class api
 		if (self::$called) return;
 		self::$called = true;
 		if ($url == null) $url = self::url();
-		if ($method == null) $method = $_SERVER["REQUEST_METHOD"];
+		if ($method == null) $method = $_SERVER['REQUEST_METHOD'];
 		$method = strtolower($method);
 		$output = self::emit($url, $method);
 		if (self::$response == null) http_response_code(200);
 		else if (is_numeric(self::$response)) http_response_code(self::$response);
 		else header(self::$response);
-		header("Content-Type: application/json; charset=utf-8");
+		header('Content-Type: application/json; charset=utf-8');
 		if ($output != null) echo json_encode($output);
 	}
 	
@@ -128,6 +128,6 @@ class api
 	}
 }
 
-register_shutdown_function(array("api", "process"));
+register_shutdown_function(array('api', 'process'));
 
 ?>
